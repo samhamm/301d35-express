296d29a538a46cd9e5aa60cf7d229ff524a99808
Sam Hamm <sam@codefellows.com>
2018-03-20 10:41:20 -0700

    Ready

---
+++ server.js
@@ -0,0 +1,24 @@
+// Code 301 Express Demo
+
+`use strict`;
+
+// Load Express
+
+// Instantiate Express so that we can use its functionality
+
+// Designate a port to serve our app on
+
+// Define which directory we will serve files from
+
+// Here is one way to set up a route... doggo...
+
+// Here is our sneaky secret 'weasels' route...
+
+// What about other methods? Can we make a POST?
+// To do so we will need to instantiate a NPM utility called 'body-parser' that is automatically installed as a dependency of Express
+
+// How will we send a POST request to this route?
+
+// Let's 404 everything except for our routes and index.html
+
+// Now let's tell the app to listen so that it can do its thing
